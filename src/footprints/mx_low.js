874427c6d8a572dc94c5724899fbd672227b8bd2
chrismelba@gmail.com <chrismelba@gmail.com>
2022-10-22 14:30:30 +1100

    upside down footprint?

--- src/footprints/mx_low.js
+++ src/footprints/mx_low.js
@@ -29,8 +29,8 @@ module.exports = {
       ${p.at /* parametric position */}
 
       ${'' /* footprint reference */}
-      (fp_text reference "${p.ref}" (at 0 -7.14375 180) ${p.ref_hide} (layer F.SilkS) (effects (font (size 1 1) (thickness 0.2))))
-      (fp_text value "" (at 0 -5.08 180) (layer F.SilkS) hide (effects (font (size 1.2 1.2) (thickness 0.2032))))
+      (fp_text reference "${p.ref}" (at 0 0) ${p.ref_hide} (layer F.SilkS) (effects (font (size 1 1) (thickness 0.2))))
+      (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.2 1.2) (thickness 0.2032))))
       ${''/* corner marks */}
       (fp_line (start 9 8.5) (end -9 8.5) (layer Dwgs.User) (width 0.1524))
       (fp_line (start -9 8.5) (end -9 -8.5) (layer Dwgs.User) (width 0.1524))
@@ -43,7 +43,7 @@ module.exports = {
       ${''/* middle shaft */}
       (pad "" np_thru_hole circle (at 0 0) (size 6.25 6.25) (drill 6.25) (layers *.Cu *.Mask))
       ${''/* stabilizers */}  
-      (pad "" np_thru_hole circle (at 0 3.7 180) (size 2.3 2.3) (drill 2.3) (layers *.Cu *.Mask))
+      (pad "" np_thru_hole circle (at 0 3.7) (size 2.3 2.3) (drill 2.3) (layers *.Cu *.Mask))
       `
     const keycap = `
       ${'' /* keycap marks */}
@@ -53,19 +53,11 @@ module.exports = {
       (fp_line (start -9 8.5) (end -9 -8.5) (layer Dwgs.User) (width 0.15))
       `
     function pins(def_neg, def_pos, def_side) {
-      if(p.param.hotswap) {
         return `
         ${''/* pins */}
-        (pad 1 thru_hole circle (at 0 6.05 180) (size 2 2) (drill 1.2) (layers *.Cu *.Mask) ${p.net.from.str})
-        (pad 2 thru_hole circle (at 4.13 3.3 41.9) (size 2 2) (drill 1.2) (layers *.Cu *.Mask) ${p.net.from.str})
+        (pad 1 thru_hole circle (at 0 6.05) (size 2 2) (drill 1.2) (layers *.Cu *.Mask) ${p.net.from.str})
+        (pad 2 thru_hole circle (at 4.13 3.3 221.9) (size 2 2) (drill 1.2) (layers *.Cu *.Mask) ${p.net.from.str})
       `
-      } else {
-          return `
-            ${''/* pins */}
-            (pad 1 thru_hole circle (at 0 6.05 180) (size 2 2) (drill 1.2) (layers *.Cu *.Mask) ${p.net.from.str})
-            (pad 2 thru_hole circle (at 4.13 3.3 41.9) (size 2 2) (drill 1.2) (layers *.Cu *.Mask) ${p.net.from.str})
-          `
-      }
     }
     if(p.param.reverse) {
       return `
