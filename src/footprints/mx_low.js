435cff88ad609739dfcddae6ba462168301fd8e6
chrismelba@gmail.com <chrismelba@gmail.com>
2022-10-22 11:55:54 +1100

    refix low

--- src/footprints/mx_low.js
+++ src/footprints/mx_low.js
@@ -18,10 +18,9 @@ module.exports = {
     to: undefined
   },
   params: {
-      class: 'S',
-      hotswap: false,
-      reverse: false,
-      keycaps: false
+    class: 'S',
+    reverse: false,
+    keycaps: false
   },
   body: p => {
     const standard = `
@@ -29,55 +28,53 @@ module.exports = {
       ${p.at /* parametric position */}
 
       ${'' /* footprint reference */}
-      (fp_text reference "Cherry MX Low Profile" (at 0 -7.14375 180) (layer Dwgs.User)
+      (fp_text reference "Cherry MX Low Profile" (at 0 -7.14375 180) (layer F.SilkS)
         (effects (font (size 1 1) (thickness 0.2)))
-      ) 
+      )
       (fp_text value MX (at 0 -5.08 180) (layer F.SilkS) hide
         (effects (font (size 1.2 1.2) (thickness 0.2032)))
       )
+
       ${''/* corner marks */}
-      (fp_line (start 6.95 6.95) (end -6.95 6.95) (layer Cmts.User) (width 0.1524))
-      (fp_line (start -6.95 6.95) (end -6.95 -6.95) (layer Cmts.User) (width 0.1524))
-      (fp_line (start -6.95 -6.95) (end 6.95 -6.95) (layer Cmts.User) (width 0.1524))
-      (fp_line (start 6.95 -6.95) (end 6.95 6.95) (layer Cmts.User) (width 0.1524))
-      (fp_line (start 9 8.5) (end -9 8.5) (layer Dwgs.User) (width 0.1524))
-      (fp_line (start -9 8.5) (end -9 -8.5) (layer Dwgs.User) (width 0.1524))
-      (fp_line (start -9 -8.5) (end 9 -8.5) (layer Dwgs.User) (width 0.1524))
-      (fp_line (start 9 -8.5) (end 9 8.5) (layer Dwgs.User) (width 0.1524))
-      (fp_line (start 7.5 7.5) (end -7.5 7.5) (layer Eco2.User) (width 0.1524))
-      (fp_line (start -7.5 7.5) (end -7.5 -7.5) (layer Eco2.User) (width 0.1524))
-      (fp_line (start -7.5 -7.5) (end 7.5 -7.5) (layer Eco2.User) (width 0.1524))
-      (fp_line (start 7.5 -7.5) (end 7.5 7.5) (layer Eco2.User) (width 0.1524))
+      (fp_line (start 6.95 6.95) (end -6.95 6.95) (layer Dwgs.User) (width 0.15))
+      (fp_line (start -6.95 6.95) (end -6.95 -6.95) (layer Dwgs.User) (width 0.15))
+      (fp_line (start -6.95 -6.95) (end 6.95 -6.95) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 6.95 -6.95) (end 6.95 6.95) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 9 8.5) (end -9 8.5) (layer Dwgs.User) (width 0.15))
+      (fp_line (start -9 8.5) (end -9 -8.5) (layer Dwgs.User) (width 0.15))
+      (fp_line (start -9 -8.5) (end 9 -8.5) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 9 -8.5) (end 9 8.5) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 7.5 7.5) (end -7.5 7.5) (layer Dwgs.User) (width 0.15))
+      (fp_line (start -7.5 7.5) (end -7.5 -7.5) (layer Dwgs.User) (width 0.15))
+      (fp_line (start -7.5 -7.5) (end 7.5 -7.5) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 7.5 -7.5) (end 7.5 7.5) (layer Dwgs.User) (width 0.15))
+
       ${''/* middle shaft */}
       (pad "" np_thru_hole circle (at 0 0 180) (size 6.25 6.25) (drill 6.25) (layers *.Cu *.Mask))`
+      
+
+      function pins(def_neg, def_pos, def_side) {
+
+          return `
+              ${''/* pins */}
+              (pad 1 thru_hole circle (at ${def_pos}0 6.05 180) (size 2 2) (drill 1.2) (layers *.Cu *.Mask) ${p.net.from.str})
+              (pad 2 thru_hole circle (at ${def_neg}4.13 3.3 41.9) (size 2 2) (drill 1.2) (layers *.Cu *.Mask) ${p.net.to.str})
+            `
+        
+      }
 
     const keycap = `
       ${'' /* keycap marks */}
-      (fp_line (start -9.5 -9.5) (end 9.5 -9.5) (layer Dwgs.User) (width 0.15))
-      (fp_line (start 9.5 -9.5) (end 9.5 9.5) (layer Dwgs.User) (width 0.15))
-      (fp_line (start 9.5 9.5) (end -9.5 9.5) (layer Dwgs.User) (width 0.15))
-      (fp_line (start -9.5 9.5) (end -9.5 -9.5) (layer Dwgs.User) (width 0.15))
-      `
-    function pins(def_neg, def_pos, def_side) {
-      return `
-        ${''/* pins */}
-        (pad 1 thru_hole circle (at 0 ${def_pos}6.05 180) (size 2 2) (drill 1.2) (layers *.Cu *.Mask) ${p.net.from.str})
-        (pad 2 thru_hole circle (at 4.13 ${def_neg}3.3 41.9) (size 2 2) (drill 1.2) (layers *.Cu *.Mask) ${p.net.to.str})
-         `
-    }
-    if(p.param.reverse){
-      return `
-        ${standard}
-        ${p.param.keycaps ? keycap : ''}
-        ${pins('-', '', 'B')}
-        ${pins('', '-', 'F')})
-        `
-    } else {
-      return `
-        ${standard}
-        ${p.param.keycaps ? keycap : ''}
-        ${pins('-', '', 'B')})
-        `
-    }
+      (fp_line (start -9 -8.5) (end 9 -8.5) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 9 -8.5) (end 9 8.5) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 9 8.5) (end -9 8.5) (layer Dwgs.User) (width 0.15))
+      (fp_line (start -9 8.5) (end -9 -8.5) (layer Dwgs.User) (width 0.15))
+      `   
+
+  return `
+    ${ standard }
+    ${ p.param.keycaps ? keycap : '' }
+    ${ pins('-', '', 'B') })
+    `
   }
 }
